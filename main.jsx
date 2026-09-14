5659b2a90e0b8e05ae25f5e0fd7f3301ecee080d
Haweanah <ahauwa48@yahoo.com>
2026-09-14 13:57:12 +0100

    Create 16 x 16 grid layout

--- main.jsx
+++ main.jsx
@@ -1,7 +1,7 @@
 const mainDiv = document.querySelector("body > div")
 function sketchDivs() {
     
-    for(let i=0; i<255; i++) {
+    for(let i=0; i<256; i++) {
         const grid = document.createElement("div")
         grid.classList.add("ind-grid")
 
