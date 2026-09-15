7cb5350934579ccc7e9d336cbaf30c951158cf06
Haweanah <ahauwa48@yahoo.com>
2026-09-15 11:03:59 +0100

    Toggle grid styles

--- main.jsx
+++ main.jsx
@@ -4,14 +4,14 @@ const btnDiv = document.querySelector(".btn-div")
 const popup = document.querySelector(".popup")
 body.appendChild(btnDiv)
 btn.textContent = "Choose a grid style"
-const mainDiv = document.querySelector("body > div")
+const mainDiv = document.querySelector(".main-div")
 
 btn.addEventListener("click", () => {
     popup.classList.toggle("show")
 })
-function sketchDivs() {
+function sketchDivs(size) {
     
-    for(let i=0; i<256; i++) {
+    for(let i=0; i< size * size; i++) {
         const grid = document.createElement("div")
         grid.classList.add("ind-grid")
 
@@ -22,6 +22,16 @@ function sketchDivs() {
         })
     }
 }
-sketchDivs();
+sketchDivs(16);
 console.log("testing")
 
+const gridBtn = document.querySelectorAll(".popup button");
+
+gridBtn.forEach(button => {
+    button.addEventListener("click", () => {
+        const size = button.dataset.size
+        mainDiv.innerHTML = ""
+        sketchDivs(size)
+        popup.classList.remove("show")
+    })
+})
