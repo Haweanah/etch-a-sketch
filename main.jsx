b0d5af8fc2c70504b5bbabcff0c6eb7241170c43
Haweanah <ahauwa48@yahoo.com>
2026-09-15 11:31:31 +0100

    Debug grid display - Recalculate flex-basis - Recalculate size of grid in function block -  Change hover property

--- main.jsx
+++ main.jsx
@@ -22,15 +22,20 @@ function sketchDivs(size) {
         })
     }
 }
+mainDiv.style.setProperty("--grid-size", 16);
+
 sketchDivs(16);
 console.log("testing")
 
 const gridBtn = document.querySelectorAll(".popup button");
 
+
 gridBtn.forEach(button => {
     button.addEventListener("click", () => {
         const size = button.dataset.size
+        console.log("Selected size:", size);
         mainDiv.innerHTML = ""
+        mainDiv.style.setProperty("--grid-size", size);
         sketchDivs(size)
         popup.classList.remove("show")
     })
